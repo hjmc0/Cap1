86ac38f3bbdeadc865ddbccc8b0cf992f815d7c5
hjmc0 <hjmc0@outlook.com>
2023-08-17 16:28:19 +0800

    Updated FireStore

--- capreact/src/Components/registration.js
+++ capreact/src/Components/registration.js
@@ -1,107 +1,184 @@
 import React, { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import { db } from "../firebase/firebase.config";
 
+const collectionRef = collection(db, "userdata");
 
 function Registration() {
-    const person = {
-        email:``,
-        password:``,
-        firstName:``,
-        lastName:``, 
-        address:``,
-        contactNum:``,
-        nric:``,
-        dateOfBirth:``
+  const person = {
+    email: "",
+    password: "",
+    cpassword: "",
+    firstName: "",
+    lastName: "",
+    address: "",
+    contactNum: "",
+    nric: "",
+    dateOfBirth: "",
+  };
+
+  const [inputData, setInputData] = useState(person);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate password confirmation
+    if (inputData.password !== inputData.cpassword) {
+      console.error("Passwords do not match");
+      return;
     }
 
-    const [inputData, setInputData] = useState(person);
-    const [submittedData, setSubmittedData] = useState(null); // Store submit data
-
-    const handleSubmit = (e) => 
-    {
-        e.preventDefault();
-        setSubmittedData(inputData); // Save submitted data
-        setInputData(person)
+    try {
+      await addDoc(collectionRef, {
+        userdata: inputData,
+      });
+      setInputData(person);
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
+  };
 
-    const handleData = (e) =>
-    {
-        setInputData({...inputData, [e.target.name]:e.target.value})
-    }
+  const handleData = (e) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value });
+  };
 
+  const handleBackToLogin = () => {
+    // Add your navigation logic here
+    // For example, use react-router to navigate to the login page
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h1>Registration:</h1>
-                <table>
-                    <tr>
-                        <td><label>Name: </label></td>
-                        <td><input type="text" name="firstName" value={inputData.firstName} onChange={handleData} placeholder="First Name"></input></td>
-                        <td><input type="text" name="lastName" value={inputData.lastName} onChange={handleData} placeholder="Last Name"></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Enter email: </label></td>
-                        <td><input type="email" name="email" value={inputData.email} onChange={handleData} placeholder="example@email.com"></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Enter password: </label></td>
-                        <td><input type="password" name="password" value={inputData.password} onChange={handleData}></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Confirm password: </label></td>
-                        <td><input type="password"  value={''} onChange={handleData}></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Address: </label></td>
-                        <td><input type="text" name="address" value={inputData.address} onChange={handleData}></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Contact Number: </label></td>
-                        <td><input type="text" name="contactNum" value={inputData.contactNum} onChange={handleData}></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>NIRC: </label></td>
-                        <td><input type="text" name="nric" value={inputData.nric} onChange={handleData}></input></td>
-                    </tr>
-                    <tr>
-                        <td><label>Date of Birth: </label></td>
-                        <td><input type="date" name="dateOfBirth" value={inputData.dateOfBirth} onChange={handleData}></input></td>
-                    </tr>
-                    <button type="Submit">Submit</button>
-                </table> 
-            </form>
-        <button>Back to Login</button>
-
-            {submittedData && (
-                <div>
-                    <h2>Submitted Data:</h2>
-                    <p>Welcome {submittedData.firstName} {submittedData.lastName}, you have successfully created an account with {submittedData.email} </p>
-                    <p>Address: {submittedData.address}</p>
-                    <p>Contact Number: {submittedData.contactNum}</p>
-                    <p>Date of Birth: {submittedData.dateOfBirth}</p>
-                    <p>NRIC: {submittedData.nric}</p>
-                    
-                    
-                </div>
-            )}
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Registration:</h1>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <label>Name: </label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="firstName"
+                  value={inputData.firstName}
+                  onChange={handleData}
+                  placeholder="First Name"
+                />
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="lastName"
+                  value={inputData.lastName}
+                  onChange={handleData}
+                  placeholder="Last Name"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter email: </label>
+              </td>
+              <td>
+                <input
+                  type="email"
+                  name="email"
+                  value={inputData.email}
+                  onChange={handleData}
+                  placeholder="example@email.com"
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Enter password: </label>
+              </td>
+              <td>
+                <input
+                  type="password"
+                  name="password"
+                  value={inputData.password}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Confirm password: </label>
+              </td>
+              <td>
+                <input
+                  type="password"
+                  name="cpassword"
+                  value={inputData.cpassword}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Address: </label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="address"
+                  value={inputData.address}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Contact Number: </label>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  name="contactNum"
+                  value={inputData.contactNum}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>NRIC: </label>
+              </td>
+              <td>
+                <input
+                  type="text"
+                  name="nric"
+                  value={inputData.nric}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Date of Birth: </label>
+              </td>
+              <td>
+                <input
+                  type="date"
+                  name="dateOfBirth"
+                  value={inputData.dateOfBirth}
+                  onChange={handleData}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+                <button type="submit">Submit</button>
+                <button onClick={handleBackToLogin}>Back to Login</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
 }
 
 export default Registration;
-
-
-            /* <h1>Registration</h1>
-            <label>Enter email: </label>
-            <input type="email" name="email" value={inputData.email} onChange={handleData}></input>
-            <br></br>
-            <label>Enter password: </label>
-            <input type="text" name="password" value={inputData.password} onChange={handleData}></input>
-            <br></br>
-            <label>First Name: </label>
-            <input type="text" name="firstName" value={inputData.firstName} onChange={handleData}></input>
-            <br></br>
-            <label>Last Name: </label>
-            <input type="text" name="lastName" value={inputData.lastName} onChange={handleData}></input>
-            <br></br>
-            <button onClick={handleSubmit}>Submit</button> */
---
+++ capreact/src/firebase/firebase.config.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Using firestore as db
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5dsHW-qXjyWZnV3dAv_5ba4R6DEsegu4",
+  authDomain: "cap1-da4a1.firebaseapp.com",
+  projectId: "cap1-da4a1",
+  storageBucket: "cap1-da4a1.appspot.com",
+  messagingSenderId: "500892811958",
+  appId: "1:500892811958:web:7a7dd364d89d75a3179c17",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Using firestore as db
+export const db = getFirestore(app);
