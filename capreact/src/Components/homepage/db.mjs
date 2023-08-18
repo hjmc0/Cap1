bc74fd5a13983e6b95dccc47a2eb7bc7409d2c9a
ljinan <87352325+ljinan@users.noreply.github.com>
2023-08-18 10:59:33 +0800

    updating homepage

--- capreact/src/Components/homepage/db.mjs
+++ capreact/src/Components/homepage/db.mjs
@@ -1,22 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; 
 
-let db = false;
+var firebaseConfig = {
+    apiKey: "AIzaSyD5dsHW-qXjyWZnV3dAv_5ba4R6DEsegu4",
+    authDomain: "cap1-da4a1.firebaseapp.com",
+    projectId: "cap1-da4a1",
+    storageBucket: "cap1-da4a1.appspot.com",
+    messagingSenderId: "500892811958",
+    appId: "1:500892811958:web:7a7dd364d89d75a3179c17"
+  };
+  
+const firebaseApp=initializeApp(firebaseConfig);
+const db=getFirestore();
 
-const getDb = () => {
-    if(!db){
-        const firebaseConfig = {
-            apiKey: "AIzaSyD5dsHW-qXjyWZnV3dAv_5ba4R6DEsegu4",
-            authDomain: "cap1-da4a1.firebaseapp.com",
-            projectId: "cap1-da4a1",
-            storageBucket: "cap1-da4a1.appspot.com",
-            messagingSenderId: "500892811958",
-            appId: "1:500892811958:web:7a7dd364d89d75a3179c17"
-        };
-
-        const app = initializeApp(firebaseConfig)
-        db = getFirestore(app)
-    }
-    return db
-}
-export {getDb};
+export default db;
