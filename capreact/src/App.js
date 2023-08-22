3d412831e636ce3f0091afd07b9811c51fb321b1
zhengxiangxp <54161150+zhengxiangxp@users.noreply.github.com>
2023-08-22 10:28:49 +0800

    dashboard

--- capreact/src/App.js
+++ capreact/src/App.js
@@ -5,12 +5,14 @@ import Home from "./Components/home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Edit from "./Components/edit";
 import Confirmation from "./Components/confirmation";
+import Dashboard from "./Components/transaction";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/dashboard' element={<Dashboard />}/>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
           <Route path="/home" element={<Home />} />
