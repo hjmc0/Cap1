aa52b24408283cba77886cbcaee04da6f9dd99f6
hjmc0 <hjmc0@outlook.com>
2023-08-23 21:56:24 +0800

    localStorage updates

--- capreact/src/Components/confirmation.js
+++ capreact/src/Components/confirmation.js
@@ -1,201 +1,209 @@
-import * as React from 'react';
+import * as React from "react";
 import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase/firebase.config";
 import { useNavigate } from "react-router-dom";
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Paper from '@mui/material/Paper';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import StepIcon from '@mui/material/StepIcon';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
-import Review from './Review';
-import Particulars from './particulars';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Toolbar from "@mui/material/Toolbar";
+import Paper from "@mui/material/Paper";
+import Stepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import StepIcon from "@mui/material/StepIcon";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+import Review from "./Review";
+import Particulars from "./particulars";
 
 //Toastify imports---------------------------------------------------
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
-const steps = ['Particulars', 'Confirmation'];
-
+const steps = ["Particulars", "Confirmation"];
 
 export default function Confirmation() {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    cpassword: "",
+    firstName: "",
+    lastName: "",
+    address: "",
+    contactNum: "",
+    nric: "",
+    dateOfBirth: "",
+    transactionDetails: [
+      { description: "UOB", date: "", amount: 10000, newBalance: 10000 },
+    ],
+  });
+  const navigate = useNavigate();
 
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [user, setUser] = React.useState({
-        email: "",
-        password: "",
-        cpassword: "",
-        firstName: "",
-        lastName: "",
-        address: "",
-        contactNum: "",
-        nric: "",
-        dateOfBirth: "",
-        transactionDetails: [{description: "UOB", date:"", amount:10000, newBalance:10000}]
-    })
-    const navigate = useNavigate();
+  const handleBackToLogin = () => {
+    localStorage.removeItem("user");
+    navigate("/");
+  };
 
-      const handleBackToLogin = () => {
-        localStorage.removeItem("user");
-        navigate("/");
-     };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const inputData = JSON.parse(localStorage.getItem("user"));
+    const collectionRef = collection(db, "userdata");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const inputData = JSON.parse(localStorage.getItem("user"))
-        const collectionRef = collection(db, "userdata");
-
-        try {
-            await addDoc(collectionRef, inputData);
-            // toast.success("Registration Success!", {
-            //     position: toast.POSITION.TOP_CENTER,
-            // });
-
-        } catch (error) {
-            console.error("Error adding document: ", error);
-        }
-        localStorage.removeItem("user");
-        toast.success("Registration Success! Loading Login Page in ...", {
-            position: toast.POSITION.TOP_CENTER,
-          })
-        setTimeout(() => {
-            navigate("/Cap1");
-        }, 3000);
-
-        setActiveStep(activeStep + 1);
-    };
-
-    function getStepContent(step) {
-        switch (step) {
-            case 0:
-                return <Particulars {...user} />
-            case 1:
-                return <Review />;
-            default:
-                throw new Error('Unknown step');
-        }
+    try {
+      await addDoc(collectionRef, inputData);
+      // toast.success("Registration Success!", {
+      //     position: toast.POSITION.TOP_CENTER,
+      // });
+    } catch (error) {
+      console.error("Error adding document: ", error);
     }
+    localStorage.removeItem("user");
+    toast.success("Registration Success! Loading Login Page in ...", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
 
-    const handleNext = () => {
-        const inputData = JSON.parse(localStorage.getItem("user"))
-        const isValidEmail = inputData.email.includes("@");
-        
-        // Validate password confirmation
-        if (inputData.password !== inputData.cpassword) {
-            toast.error("Check your password again!", {
-                position: toast.POSITION.TOP_CENTER,
-            });
-        } else if (inputData.email === "" || !isValidEmail) {
-            toast.error("Invalid Email!", {
-                position: toast.POSITION.TOP_CENTER,
-            });
-        } else if (inputData.password === "") {
-            toast.error("Invalid Password", {
-                position: toast.POSITION.TOP_CENTER,
-            });
-        } else {
-            setActiveStep(activeStep + 1);
-        }
-    };
+    setActiveStep(activeStep + 1);
+  };
 
-    const handleBack = () => {
-        const newUser = JSON.parse(localStorage.getItem("user"))
-        setUser(newUser)
-        setActiveStep(activeStep - 1);
-    };
+  function getStepContent(step) {
+    switch (step) {
+      case 0:
+        return <Particulars {...user} />;
+      case 1:
+        return <Review />;
+      default:
+        throw new Error("Unknown step");
+    }
+  }
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            height: '120vh',
-            backgroundImage: 'url(https://www.pixelstalk.net/wp-content/uploads/images1/Beautiful-singapore-hd-wallpapers.jpg)',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-        }}>
+  const handleNext = () => {
+    const inputData = JSON.parse(localStorage.getItem("user"));
+    const isValidEmail = inputData.email.includes("@");
 
-            <React.Fragment >
-                <Container component="main" maxWidth="md" >
-                    <Paper variant="outlined" sx={{
-                        pl: 5, pr: 5, justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '110vh',
-                    }} style={{ backgroundColor: 'lightgrey' }}>
+    // Validate password confirmation
+    if (inputData.password !== inputData.cpassword) {
+      toast.error("Check your password again!", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    } else if (inputData.email === "" || !isValidEmail) {
+      toast.error("Invalid Email!", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    } else if (inputData.password === "") {
+      toast.error("Invalid Password", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    } else {
+      setActiveStep(activeStep + 1);
+    }
+  };
 
-                        <Stepper activeStep={activeStep} sx={{ pt: 5, pb: 6 }} style={{ scale: '160%', marginLeft: '200px', marginRight: '200px' }}>
-                            {steps.map((label) => (
-                                <Step key={label} >
-                                    <StepLabel style={{ fontSize: '20px' }}>
-                                        {label}
-                                    </StepLabel>
-                                </Step>
-                            ))}
-                        </Stepper>
-                        {activeStep === steps.length ? (
-                            <React.Fragment>
-                                <Typography variant="h5" gutterBottom>
-                                    Thank you for registering.
-                                </Typography>
-                                <Typography variant="subtitle1">
-                                    Your account has been recorded. Redirecting to Login Page...
-                                </Typography>
-                                <br />
-                                <Button href="/Cap1" variant="contained">
-                                    Go to Login
-                                </Button>
-                            </React.Fragment>
-                        ) : (
-                            <React.Fragment>
-                                {getStepContent(activeStep)}
-                                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                                    {activeStep !== 0 && (
-                                        <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                                            Back
-                                        </Button>
-                                    )}
+  const handleBack = () => {
+    const newUser = JSON.parse(localStorage.getItem("user"));
+    setUser(newUser);
+    setActiveStep(activeStep - 1);
+  };
 
-                                    {activeStep === steps.length - 1 && (
-                                        <Button onClick={handleSubmit} sx={{ mt: 3, ml: 1 }}>
-                                            Submit
-                                        </Button>
-                                    )}
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "120vh",
+        backgroundImage:
+          "url(https://www.pixelstalk.net/wp-content/uploads/images1/Beautiful-singapore-hd-wallpapers.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <React.Fragment>
+        <Container component="main" maxWidth="md">
+          <Paper
+            variant="outlined"
+            sx={{
+              pl: 5,
+              pr: 5,
+              justifyContent: "center",
+              alignItems: "center",
+              height: "110vh",
+            }}
+            style={{ backgroundColor: "lightgrey" }}
+          >
+            <Stepper
+              activeStep={activeStep}
+              sx={{ pt: 5, pb: 6 }}
+              style={{
+                scale: "160%",
+                marginLeft: "200px",
+                marginRight: "200px",
+              }}
+            >
+              {steps.map((label) => (
+                <Step key={label}>
+                  <StepLabel style={{ fontSize: "20px" }}>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            {activeStep === steps.length ? (
+              <React.Fragment>
+                <Typography variant="h5" gutterBottom>
+                  Thank you for registering.
+                </Typography>
+                <Typography variant="subtitle1">
+                  Your account has been recorded. Redirecting to Login Page...
+                </Typography>
+                <br />
+                <Button href="/Cap1" variant="contained">
+                  Go to Login
+                </Button>
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                {getStepContent(activeStep)}
+                <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                  {activeStep !== 0 && (
+                    <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                      Back
+                    </Button>
+                  )}
 
-                                     {activeStep !== steps.length - 1 && (
-                                        <Button onClick={handleBackToLogin} sx={{ mt: 3, ml: 1 }}>
-                                            Back to Login
-                                        </Button>
-                                    )}
+                  {activeStep === steps.length - 1 && (
+                    <Button onClick={handleSubmit} sx={{ mt: 3, ml: 1 }}>
+                      Submit
+                    </Button>
+                  )}
 
-                                    {activeStep !== steps.length - 1 && (
-                                        <Button onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
-                                            Next
-                                        </Button>
-                                    )}
-                                   
-                                   
+                  {activeStep !== steps.length - 1 && (
+                    <Button onClick={handleBackToLogin} sx={{ mt: 3, ml: 1 }}>
+                      Back to Login
+                    </Button>
+                  )}
 
-                                    {/* <Button
+                  {activeStep !== steps.length - 1 && (
+                    <Button onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
+                      Next
+                    </Button>
+                  )}
+
+                  {/* <Button
                                         variant="contained"
                                         onClick={handleNext}
                                         sx={{ mt: 3, ml: 1 }}
                                     >
                                         {activeStep === steps.length - 1 ? 'Submit' : 'Next'}
                                     </Button> */}
-                                </Box>
-                            </React.Fragment>
-                        )}
-                    <ToastContainer />
-                    </Paper>
-
-                </Container>
-            </React.Fragment>
-        </Box >
-    );
+                </Box>
+              </React.Fragment>
+            )}
+            <ToastContainer />
+          </Paper>
+        </Container>
+      </React.Fragment>
+    </Box>
+  );
 }
